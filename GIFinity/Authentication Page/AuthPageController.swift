19823846e7db4b44b875603d4d65e2720b3c9f4e
Ziyadkhan Taghiyev <ziyadxan.tagiyev@gmail.com>
2024-01-09 15:53:34 +0400

    Commit

--- GIFinity/Authentication Page/AuthPageController.swift
+++ GIFinity/Authentication Page/AuthPageController.swift
@@ -8,15 +8,36 @@
 import UIKit
 
 class AuthPageController: UIViewController {
+    
     @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var authSegmentOutlet: UISegmentedControl!
+    @IBOutlet weak var signupSegment: UIView!
+    @IBOutlet weak var loginSegment: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configUI()
-
-        // Do any additional setup after loading the view.
+        configSegment()
     }
-  
+    
+    @IBAction func segmentSelection(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.view.bringSubviewToFront(loginSegment)
+            loginSegment.isHidden = false
+            signupSegment.isHidden = true
+            sender.selectedSegmentTintColor = .buttonColour
+        case 1:
+            self.view.bringSubviewToFront(signupSegment)
+            signupSegment.isHidden = false
+            loginSegment.isHidden = true
+            sender.selectedSegmentTintColor = .signupSelection
+        default:
+            break
+        }
+    }
+    
 }
 
 extension AuthPageController {
@@ -24,5 +45,14 @@ extension AuthPageController {
         let backgroundGif = UIImage.gifImageWithName("background")
         background.image = backgroundGif
         self.navigationItem.setHidesBackButton(true, animated: true)
+    }
+    
+    func configSegment() {
+        self.view.bringSubviewToFront(signupSegment)
+        authSegmentOutlet.selectedSegmentIndex = 1 // kecid edende ilk bashda sign up tovsiyye etsin
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        authSegmentOutlet.setTitleTextAttributes(titleTextAttributes, for: .normal)
+        signupSegment.isHidden = false
+        loginSegment.isHidden = true
     }
 }
