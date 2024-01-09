7a0660587cdf7f23d9df60380dc29614bde0d2f0
Ziyadkhan Taghiyev <ziyadxan.tagiyev@gmail.com>
2024-01-09 19:39:50 +0400

    Commit

--- GIFinity/Authentication Page/Segment Controller Page/LoginSegmentController.swift
+++ GIFinity/Authentication Page/Segment Controller Page/LoginSegmentController.swift
@@ -9,21 +9,40 @@ import UIKit
 
 class LoginSegmentController: UIViewController {
 
+    @IBOutlet weak var loginEmailTextField: UITextField!
+    @IBOutlet weak var loginPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func forgotPassButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signinFacebook(_ sender: Any) {
+    }
+    
+    @IBAction func signinApple(_ sender: Any) {
+    }
+    
+    @IBAction func privacyAndPolicy(_ sender: Any) {
+        if let url = URL(string: "https://support.giphy.com/hc/en-us/articles/360032872931") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func termsOfService(_ sender: Any) {
+        if let url = URL(string: "https://support.giphy.com/hc/en-us/articles/360020027752-GIPHY-User-Terms-of-Service#:~:text=Please%20do%20not%20publicly%20post,in%20connection%20with%20its%20Services.") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    
 }
--- GIFinity/Authentication Page/Segment Controller Page/RegisterSegmentController.swift
+++ GIFinity/Authentication Page/Segment Controller Page/RegisterSegmentController.swift
@@ -9,21 +9,52 @@ import UIKit
 
 class RegisterSegmentController: UIViewController {
 
+    @IBOutlet weak var regEmailTextField: UITextField!
+    
+    @IBOutlet weak var regUsernameTextField: UITextField!
+    
+    @IBOutlet weak var regPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signupButton(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func alreadyStartedRegButton(_ sender: Any) {
+    }
+    
+    @IBAction func signinFacebook(_ sender: Any) {
+    }
+    
+    @IBAction func signinApple(_ sender: Any) {
+    }
+    
+    @IBAction func termsOfService(_ sender: Any) {
+        if let url = URL(string: "https://support.giphy.com/hc/en-us/articles/360020027752-GIPHY-User-Terms-of-Service#:~:text=Please%20do%20not%20publicly%20post,in%20connection%20with%20its%20Services.") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func privacyPolicy(_ sender: Any) {
+        if let url = URL(string: "https://support.giphy.com/hc/en-us/articles/360032872931") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func googlePrivacy(_ sender: Any) {
+        if let url = URL(string: "https://policies.google.com/privacy?hl=en-US") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func tickButton(_ sender: UIButton) {
+        if sender.isSelected {
+            sender.isSelected = false
+        } else {
+            sender.isSelected = true
+        }
+    }
 }
